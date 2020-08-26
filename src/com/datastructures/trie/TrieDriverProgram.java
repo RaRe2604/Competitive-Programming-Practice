da7cc47c1e7ec22fe772b12e965b3fc8860e5089
RaRe2604 <k.rahul2604@gmail.com>
2020-08-26 16:08:23 +0530

    Update TrieDriverProgram.java
    Changes made to data inserted and searched in Trie

--- src/com/datastructures/trie/TrieDriverProgram.java
+++ src/com/datastructures/trie/TrieDriverProgram.java
@@ -3,12 +3,15 @@ package com.datastructures.trie;
 public class TrieDriverProgram {
 	public static void main(String[] args) {
 		Trie trie = new Trie();
-		trie.insert("rahul");
-		trie.insert("demo");
-		trie.insert("dex");
-		trie.insert("dexter");
-		trie.insert("demoed");
-		System.out.println(trie.search("ra"));
-		System.out.println(trie.startsWith("rax"));
+		trie.insert("well");
+		trie.insert("welcome");
+		trie.insert("datastructure");
+		trie.insert("data");
+		trie.insert("structure");
+		trie.insert("java");
+		trie.insert("backend");
+		trie.insert("javadeveloper");
+		System.out.println(trie.search("java"));
+		System.out.println(trie.startsWith("dat"));
 	}
 }
