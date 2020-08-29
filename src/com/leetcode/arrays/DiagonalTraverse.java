8a996a97629642e33c9a6ccc0a297b80338d3e50
Rahul Reddy <k.rahul2604@gmail.com>
2020-08-29 09:28:43 +0530

    Leetcode - Arrays and String - Introduction to 2D Array

--- src/com/leetcode/arrays/DiagonalTraverse.java
+++ src/com/leetcode/arrays/DiagonalTraverse.java
@@ -4,7 +4,37 @@ import java.util.stream.IntStream;
 
 public class DiagonalTraverse {
     public static int[] findDiagonalOrder(int[][] matrix) {
-        int[] result = new int[matrix.length * 2];
+        int rows = matrix.length;
+        int column = matrix[0].length;
+        int length = rows * column;
+        int[] result = new int[length];
+        boolean up = true;
+        for (int index = 0, i = 0, j = 0; index < length; index++) {
+            result[index] = matrix[i][j];
+            if (up) {
+                if (j == column - 1) {
+                    i++;
+                    up = false;
+                } else if (i == 0) {
+                    j++;
+                    up = false;
+                } else {
+                    i--;
+                    j++;
+                }
+            } else {
+                if (i == rows - 1) {
+                    j++;
+                    up = true;
+                } else if (j == 0) {
+                    i++;
+                    up = true;
+                } else {
+                    i++;
+                    j--;
+                }
+            }
+        }
         return result;
     }
 
@@ -12,7 +42,6 @@ public class DiagonalTraverse {
         int[][] input = {{1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}};
-        IntStream.of(findDiagonalOrder(input)).forEach(System.out::println);
+        IntStream.of(findDiagonalOrder(input)).forEach(System.out::print);
     }
-
 }
