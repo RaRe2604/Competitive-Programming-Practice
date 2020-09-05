e6e05aefa8b47ee89340fb1b82afa9f3c646d703
Rahul Reddy <k.rahul2604@gmail.com>
2020-09-05 16:13:46 +0530

    HackerEarth - Code Monk - Arrays

--- src/main/java/com/hackerearth/codemonk/MonkAndRotation.java
+++ src/main/java/com/hackerearth/codemonk/MonkAndRotation.java
@@ -1,5 +1,7 @@
 package com.hackerearth.codemonk;
 
+import java.util.Scanner;
+
 /**
  * User: Rahul Reddy
  * Date: 9/5/2020
@@ -7,4 +9,22 @@ package com.hackerearth.codemonk;
  */
 
 public class MonkAndRotation {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int testcases = scan.nextInt();
+        StringBuilder builder = new StringBuilder();
+        while (testcases-- > 0) {
+            int length = scan.nextInt();
+            int rotate = scan.nextInt();
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++) {
+                array[(i + rotate) % length] = scan.nextInt();
+            }
+            for (int i = 0; i < length; i++) {
+                builder.append(array[i]).append(" ");
+            }
+            builder.append("\n");
+        }
+        System.out.print(builder);
+    }
 }
